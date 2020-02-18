79041559904ae0f13b9dfff2ce87633d919dceee
Darren Ford <dford_au-reg@yahoo.com>
2020-02-18 19:08:59 +1100

    Added objc demo

---
+++ Demos/DSFFloatLabelledTextControls Demo objc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DSFFloatLabelledTextControls Demo objc
+//
+//  Created by Darren Ford on 18/2/20.
+//  Copyright © 2020 Darren Ford. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Demos/DSFFloatLabelledTextControls Demo objc/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  DSFFloatLabelledTextControls Demo objc
+//
+//  Created by Darren Ford on 18/2/20.
+//  Copyright © 2020 Darren Ford. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
